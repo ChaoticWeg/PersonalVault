ffd273b3e0087bb0dbd2e84d58445e643b791aab
ChaoticWeg <shawn@chaoticweg.cc>
2019-05-27 00:40:54 -0500

    change material to name (string) and write deserialization code

--- src/main/java/cc/chaoticweg/mc/personalvault/serialization/ItemStackHandler.java
+++ src/main/java/cc/chaoticweg/mc/personalvault/serialization/ItemStackHandler.java
@@ -1,15 +1,13 @@
 package cc.chaoticweg.mc.personalvault.serialization;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import com.google.gson.*;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 import java.lang.reflect.Type;
 import java.util.Objects;
 
-public class ItemStackHandler implements JsonSerializer<ItemStack> {
+public class ItemStackHandler implements JsonSerializer<ItemStack>, JsonDeserializer<ItemStack> {
 
     @Override
     public JsonElement serialize(ItemStack src, Type type, JsonSerializationContext context) {
@@ -17,13 +15,24 @@ public class ItemStackHandler implements JsonSerializer<ItemStack> {
 
         // none of these SHOULD ever be null, but some CAN be. whew.
         try {
+            json.addProperty("material", Objects.requireNonNull(src.getData()).getItemType().name());
             json.addProperty("amount", src.getAmount());
-            json.addProperty("material", Objects.requireNonNull(src.getData()).getItemType().ordinal());
-        } catch (NullPointerException e) {
+        }
+        catch (NullPointerException e) {
             return null;
         }
 
         return json;
     }
 
+    @Override
+    public ItemStack deserialize(JsonElement elem, Type type, JsonDeserializationContext context)
+            throws JsonParseException {
+        JsonObject obj = elem.getAsJsonObject();
+
+        Material material = Material.valueOf(obj.get("material").getAsString());
+        int amount = obj.get("amount").getAsInt();
+
+        return new ItemStack(material, amount);
+    }
 }
