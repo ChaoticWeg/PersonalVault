65022693a2568cc9f03575ec6d5aa9c652ab63ed
ChaoticWeg <shawn@chaoticweg.cc>
2019-05-28 00:05:04 -0500

    restrict some access to inventory adapter methods

--- src/main/java/cc/chaoticweg/mc/personalvault/serialization/InventoryAdapter.java
+++ src/main/java/cc/chaoticweg/mc/personalvault/serialization/InventoryAdapter.java
@@ -7,13 +7,12 @@ public class InventoryAdapter {
 
     private static final int SLOTS_PER_ROW = 9; // CANNOT BE CHANGED OR BUKKIT WILL DIE
     private static final int NUM_ROWS = 6; // this can be changed, though probably don't do that
-    public static final int DEFAULT_NUM_SLOTS = SLOTS_PER_ROW * NUM_ROWS;
+    private static final int DEFAULT_NUM_SLOTS = SLOTS_PER_ROW * NUM_ROWS;
 
     public static Inventory createInventory(int size) {
         return Bukkit.getServer().createInventory(null, size);
     }
-
-    public static Inventory createInventory() {
+    static Inventory createInventory() {
         return createInventory(DEFAULT_NUM_SLOTS);
     }
 
